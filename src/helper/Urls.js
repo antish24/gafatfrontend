924ae1781f5b86566ee8a2cdab2d8284a01f2619
antish24 <aantenehh@gmail.com>
2024-10-08 16:29:10 -0700

    leave report url backend

--- src/helper/Urls.js
+++ src/helper/Urls.js
@@ -1,4 +1,4 @@
-// export const BACKENDURL='http://195.201.22.42:8080'
-export const BACKENDURL='http://localhost:8080'
+export const BACKENDURL='http://195.201.22.42:8080'
+// export const BACKENDURL='http://localhost:8080'
 export const BLACKLISTAPI='http://localhost:2025'
 export const UPLOADAPI='http://localhost:2024'
